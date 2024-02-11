5b2fea174faa57e88a1d464f27e0d2adb91ea85c
Santosh7017 <santoshmorya400@gmail.com>
2024-02-12 04:42:11 +0530

    UI update

--- app/components/footer/HomeBanner.tsx
+++ app/components/footer/HomeBanner.tsx
@@ -1,25 +1,55 @@
-import Image from "next/image"
+"use client"
 
+import Image from "next/image";
+import { motion } from "framer-motion";
 
 const HomeBanner = () => {
   return (
-    <div className="relative bg-gradient-to-r from-sky-500 to-sky-700 mb-8">
-        <div className="mx-auto px-8 py-12 flex flex-col gap-2 md:flex-row items-center justify-evenly">
-            <div className="mb-8 md:mb-0 text-center">
-                <h1 className="text-4x1 md:text-6xl font-bold text-white mb-4">Summer Sale</h1>
-                <p className="text-lg md:text-xl text-white mb-2">Enjoy discounts on selected items</p>
-                <p className="text-2xl md:text-5xl text-yellow-400 font-bold">GET 50% OFF</p>
-            </div>
-            <div className="w-1/3 relative aspect-video">
-                <Image src='/banner/pngwing.png' alt="Banner Image"
-                className="object-contain "
-                quality={100}
-                fill
-                />
-                 </div>
-        </div>
-    </div>
-  )
-}
+    <motion.div
+      className="relative mb-8"
+      animate={{
+        backgroundColor: ["#9c27b0", "#ffeb3b", "#8bc34a", "#4caf50", "#009688", "#00bcd4", "#03a9f4", "#3f51b5"],
+        transition: { duration: 20, ease: "linear", repeat: Infinity },
+      }}
+    >
+      <div className="mx-auto px-8 py-12 flex flex-col gap-2 md:flex-row items-center justify-evenly">
+        <motion.div
+          className="mb-8 md:mb-0 text-center"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1, transition: { duration: 0.8, ease: "easeIn" } }}
+        >
+          <motion.h1
+            className="text-4x1 md:text-6xl font-bold text-white mb-4"
+            animate={{ y: [-10, 0], transition: { duration: 0.5, ease: "easeOut" } }}
+          >
+            Summer Extravaganza
+          </motion.h1>
+          <motion.p
+            className="text-lg md:text-xl text-white mb-2"
+            animate={{ scale: [0.8, 1], transition: { duration: 0.4 } }}
+          >
+            Unbelievable discounts on our entire collection
+          </motion.p>
+          <motion.p
+            className="text-2xl md:text-5xl text-yellow-400 font-bold"
+            animate={{
+              x: [-10, 0],
+              backgroundColor: ["rgba(0, 0, 0, 0)", "rgba(0, 0, 0, 0.3)"],
+              transition: { duration: 0.7, ease: "backInOut" },
+            }}
+          >
+            UP TO 70% OFF
+          </motion.p>
+        </motion.div>
+        <motion.div
+          className="w-1/3 relative aspect-video"
+          animate={{ scale: [0.95, 1], transition: { duration: 0.4, ease: "easeInOut" } }}
+        >
+          <Image src="/banner/pngwing.png" alt="Banner Image" className="object-contain" quality={100} fill />
+        </motion.div>
+      </div>
+    </motion.div>
+  );
+};
 
-export default HomeBanner
+export default HomeBanner;
